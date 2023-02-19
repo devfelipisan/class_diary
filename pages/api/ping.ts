15693ce54f23816982f843071bd3d877e243254f
Felipi Santana <devfelipisan@gmail.com>
2023-02-19 14:43:18 -0300

    chore: refactor request test

--- pages/api/ping.ts
+++ pages/api/ping.ts
@@ -1,13 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
 
-type Data = {
-  ping: string;
-};
-
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>
-) {
-  res.status(200).json({ ping: "pong!" });
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  res.status(200).json("pong!");
 }

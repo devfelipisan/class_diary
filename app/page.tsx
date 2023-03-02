8df09d2df8f8eca5340f72c7e36a3d6fc522ac05
Felipi Santana <devfelipisan@gmail.com>
2023-03-01 22:01:14 -0300

    feat: new main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,28 @@
-"use client";
-
 import Link from "next/link";
 
-const localList = [
-  "BNCC",
-  "Estudante",
-  "Secretaria",
-  "Professor",
-  "Coordenação",
+interface menuDto {
+  id: string;
+  name: string;
+}
+
+const menu: Array<menuDto> = [
+  { id: "student", name: "Estudante" },
+  { id: "teacher", name: "Professor" },
+  { id: "secretary", name: "Secretaria" },
+  { id: "coordination", name: "Coordenação" },
 ];
 
 export default function Home() {
   return (
     <div className="flex flex-row space-x-4 mt-6 px-5">
-      {localList.map((item, index: any) => (
+      {menu.map((item: menuDto) => (
         <Link
           className="basis-1/4 hover:basis-1/3 justify-self-center"
-          href={`/enroll/${item}`}
-          key={index}
+          href={item.id}
+          key={item.id}
         >
           <div className="p-2 text-center bg-indigo-50 hover:bg-indigo-500 rounded-xl">
-            {item}
+            {item.name}
           </div>
         </Link>
       ))}

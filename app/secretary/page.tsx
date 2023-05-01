c5fa4a76da564d10e5fcf153cc4242cced32694c
Felipi Santana <devfelipisan@gmail.com>
2023-05-01 11:43:14 -0300

    chore: remove button cancel

--- app/secretary/page.tsx
+++ app/secretary/page.tsx
@@ -59,22 +59,13 @@ export default function Page() {
           </div>
         </div>
       </div>
-      <div className="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
+      <div className="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-1">
         <div className="mt-10">
           <button
             type="submit"
             className="block w-full rounded-md bg-indigo-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
           >
             Matricular
-          </button>
-        </div>
-        <div className="mt-10">
-          <button
-            type="button"
-            className="block w-full rounded-md bg-rose-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-rose-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-rose-600"
-            onClick={() => router.push("/")}
-          >
-            Cancelar
           </button>
         </div>
       </div>

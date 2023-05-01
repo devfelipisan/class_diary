548d2c0a413cc6bc845387d6e39e3471028f9028
Felipi Santana <devfelipisan@gmail.com>
2023-05-01 00:55:54 -0300

    chore: update teacher's page

--- app/teacher/page.tsx
+++ app/teacher/page.tsx
@@ -1,10 +1,35 @@
 "use client";
-import React from "react";
+import { Prisma } from "@prisma/client";
+import React, { useEffect, useState } from "react";
+import { GetTeachers } from "../../services/teachers";
 
 export default function Page() {
-  function renderConditional() {
+  const [data, setData] = useState<
+    Prisma.teachersGetPayload<{ select: { id: true; name: true } }> | any
+  >();
+
+  async function DataList() {
+    const data:
+      | Prisma.teachersGetPayload<{
+          select: { id: true; name: true };
+        }>
+      | any = await GetTeachers("/api/teachers").then((item) => {
+      return item;
+    });
+    let result = data;
+    setData(result);
+    return result;
+  }
+
+  DataList();
+
+  useEffect(() => {
+    console.log(data);
+  }, [data]);
+
+  function renderConditional(item: any) {
     return (
-      <div className="my-2 rounded-md border-1 shadow-md">
+      <div className="my-2 rounded-md border-1 shadow-md" key={item.id}>
         <div className="block rounded-md border-1 mx-10 py-2 shadow-sm">
           <div className="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-6">
             <div className="sm:col-span-3">
@@ -21,6 +46,8 @@ export default function Page() {
                   id="first-name"
                   autoComplete="given-name"
                   className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  value={item.name}
+                  disabled
                 />
               </div>
             </div>
@@ -73,7 +100,7 @@ export default function Page() {
   }
   return (
     <React.Fragment>
-      {[].map((item: any) => renderConditional())}
+      {/*data?.map((item: any) => renderConditional(item))*/}
     </React.Fragment>
   );
 }

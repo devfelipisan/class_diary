b07a942d7d0a3f8a095e0fb969060fabc0c24745
Felipi Santana <devfelipisan@gmail.com>
2023-05-01 00:50:58 -0300

    chore: update menu dashboard

--- app/navigatorSystem.tsx
+++ app/navigatorSystem.tsx
@@ -2,13 +2,46 @@
 
 import { Fragment } from "react";
 import { Disclosure } from "@headlessui/react";
+import DropBox from "./components/dropBox";
 
 const navigation = [
-  { name: "Início", href: "/", current: true },
-  { name: "Estudante", href: "/student", current: false },
-  { name: "Professor", href: "/teacher", current: false },
-  { name: "Secretaria", href: "/secretary", current: false },
-  { name: "Coordenação", href: "/coordination", current: false },
+  {
+    name: "Início",
+    href: "/",
+    current: true,
+  },
+  {
+    name: "Estudante",
+    href: "/student",
+    current: true,
+  },
+  {
+    name: "Professor",
+    href: "/teacher",
+    current: true,
+  },
+  {
+    name: "Secretaria",
+    href: "/secretary",
+    current: true,
+  },
+  {
+    name: "Coordenação",
+    href: "/",
+    current: true,
+    option: [
+      {
+        subLabel: "Cadastrar turma",
+        href: "/coordination/newclass",
+        current: false,
+      },
+      {
+        subLabel: "Cadastrar professor",
+        href: "/coordination/newteacher",
+        current: false,
+      },
+    ],
+  },
 ];
 
 function classNames(...classes: any[]) {
@@ -28,19 +61,26 @@ export default function NavigatorSystem() {
               <div className="hidden md:block">
                 <div className="ml-10 flex items-baseline space-x-4">
                   {navigation.map((item) => (
-                    <a
+                    <DropBox
                       key={item.name}
+                      name={item.name}
                       href={item.href}
-                      className={classNames(
-                        item.current
-                          ? "text-white"
-                          : "text-gray-200 hover:bg-gray-700 hover:text-white",
-                        "rounded-md px-3 py-2 text-sm font-medium"
-                      )}
-                      aria-current={item.current ? "page" : undefined}
-                    >
-                      {item.name}
-                    </a>
+                      current={item.current}
+                      option={item.option}
+                    />
+                    // <a
+                    //   key={item.name}
+                    //   href={item.href}
+                    //   className={classNames(
+                    //     item.current
+                    //       ? "text-white"
+                    //       : "text-gray-200 hover:bg-gray-700 hover:text-white",
+                    //     "rounded-md px-3 py-2 text-sm font-medium"
+                    //   )}
+                    //   aria-current={item.current ? "page" : undefined}
+                    // >
+                    //   {item.name}
+                    // </a>
                   ))}
                 </div>
               </div>

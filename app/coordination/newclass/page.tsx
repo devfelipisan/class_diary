a484efa6e37e25663e4ef1a937e95f28a4f4bc4e
Felipi Santana <devfelipisan@gmail.com>
2023-05-02 00:47:21 -0300

    chore: update new class enroll

--- app/coordination/newclass/page.tsx
+++ app/coordination/newclass/page.tsx
@@ -3,25 +3,60 @@ import React, { useCallback, useEffect, useState } from "react";
 import { Prisma } from "@prisma/client";
 import SelectMenu from "@/app/components/selectBox";
 import Modal from "@/app/components/modal";
-import TeachersList from "../../../hooks/teacherList";
+import ListBox from "@/app/components/listBox";
+import SlideOver from "@/app/components/slideOver";
+import ClassList from "@/hooks/classList";
+import TeachersList from "@/hooks/teacherList";
 
 export default function Page() {
-  const [data, setData] =
-    useState<Prisma.teachersGetPayload<{ select: { id: true; name: true } }>>();
-  const [onSelected, setOnSelected] = useState<
+  const [classData, setClassData] = useState<
+    Array<
+      Prisma.graduating_classGetPayload<{
+        select: {
+          id: true;
+          teacher_id: true;
+          teacher: {
+            select: {
+              name: true;
+              email: true;
+            };
+          };
+          school_series: true;
+          school_year: true;
+        };
+      }>
+    >
+  >();
+
+  const [listTeachers, setListTeachers] = useState<
     Prisma.teachersGetPayload<{
       select: { id: true; name: true; email: true };
     }>
+  >();
+
+  const [onSelected, setOnSelected] = useState<
+    Prisma.teachersGetPayload<{
+      select: {
+        id: true;
+        name: true;
+        email: true;
+      };
+    }>
   >({ id: "00", name: "Selecione...", email: "" });
-  const [open, setOpen] = useState(false);
+
+  const [openModal, setOpenModal] = useState(false);
+  const [openSlideOver, setOpenSlideOver] = useState(false);
 
   const load = useCallback(async () => {
-    setData(await TeachersList());
+    setClassData(await ClassList());
+    setListTeachers(await TeachersList());
   }, []);
 
   useEffect(() => {
     load();
   }, [load]);
+
+  console.log(classData);
 
   function handleSubmit(e: React.BaseSyntheticEvent) {
     fetch("/api/graduating_class", {
@@ -37,13 +72,23 @@ export default function Page() {
       }),
     });
 
-    //e.currentTarget.reset();
-    setOpen(true);
+    setOpenModal(true);
   }
 
   return (
     <>
-      <Modal open={open} setOpen={setOpen} title={"Cadastrado com Sucesso"} />
+      <Modal
+        open={openModal}
+        setOpen={setOpenModal}
+        title={"Cadastrado com Sucesso"}
+      />
+      <SlideOver
+        open={openSlideOver}
+        setOpen={setOpenSlideOver}
+        title="Listagem de professores"
+      >
+        <ListBox data={classData} />
+      </SlideOver>
       <div className="mx-auto max-w-2xl text-center">
         <p className="mt-2 text-lg leading-8 text-gray-600">
           Cadastre aqui uma nova turma
@@ -58,7 +103,7 @@ export default function Page() {
             >
               Selecione o professor titular
             </label>
-            <SelectMenu data={data} onSelected={setOnSelected} />
+            <SelectMenu data={listTeachers} onSelected={setOnSelected} />
           </div>
 
           <div className="sm:col-span-2 mt-2.5">
@@ -96,13 +141,22 @@ export default function Page() {
             </div>
           </div>
         </div>
-        <div className="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-1">
+        <div className="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
           <div className="mt-10">
             <button
               type="submit"
               className="block w-full rounded-md bg-indigo-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
             >
               Criar
+            </button>
+          </div>
+          <div className="mt-10">
+            <button
+              type="button"
+              className="block w-full rounded-md bg-green-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-green-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-green-600"
+              onClick={() => setOpenSlideOver(true)}
+            >
+              Listar professores
             </button>
           </div>
         </div>

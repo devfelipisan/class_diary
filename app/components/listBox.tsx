2e3c6bddc8279be4ee6b940ba41dd0759acdb6ed
Felipi Santana <devfelipisan@gmail.com>
2023-05-02 00:42:37 -0300

    feat: new components

---
+++ app/components/listBox.tsx
@@ -0,0 +1,52 @@
+interface teacherProps {
+  name: string;
+  email: string;
+}
+
+interface itemListBoxProps {
+  id: string;
+  school_series: string;
+  teacher_id: string;
+  teacher: teacherProps | null;
+  school_year: number;
+}
+
+interface listBoxProps {
+  data: Array<itemListBoxProps> | undefined;
+}
+
+export default function ListBox(props: listBoxProps) {
+  return (
+    // <div className="bg-gradient-to-r from-[#ffffff3d] to-[#ffffff56] rounded-lg px-6 py-24 sm:py-32 lg:px-8">
+    <div className="max-h-50 overflow-auto scroll-smooth rounded-lg mt-6 sm:py-18">
+      <ul role="list" className="divide-y divide-gray-100">
+        {props?.data?.map((person) => (
+          <li
+            onClick={() => console.log(person.teacher_id)}
+            key={person.id}
+            className="flex justify-between gap-x-6 py-5 px-8 cursor-pointer rounded-lg transition duration-500 hover:bg-green-100 hover:text-green-500 focus:bg-green-100 focus:text-green-500 focus:ring-0 dark:hover:bg-green-600 dark:hover:text-green-200 dark:focus:bg-green-600 dark:focus:text-green-200"
+          >
+            <div className="flex gap-x-4">
+              <div className="min-w-0 flex-auto">
+                <p className="text-sm font-semibold leading-6 text-gray-900">
+                  {person.teacher?.name}
+                </p>
+                <p className="mt-1 truncate text-xs leading-5 text-gray-500">
+                  {person.teacher?.email}
+                </p>
+              </div>
+            </div>
+            <div className="hidden sm:flex sm:flex-col sm:items-end">
+              <p className="text-sm leading-6 text-gray-900">
+                {`Ano escolar ${person.school_year}`}
+              </p>
+              <p className="mt-1 text-xs leading-5 text-gray-500">
+                {`Turma ${person.school_series}`}
+              </p>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}

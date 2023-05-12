48d7691462ee4a3628284f85435ad31b192b76db
Felipi Santana <devfelipisan@gmail.com>
2023-05-11 23:21:05 -0300

    chore: adjust db model

--- prisma/migrations/20230512021839_202305112318/migration.sql
+++ prisma/migrations/20230512021839_202305112318/migration.sql
@@ -24,7 +24,7 @@ CREATE TABLE "coefficients" (
     "student_id" TEXT NOT NULL,
     "teacher_id" TEXT NOT NULL,
     "competence_id" TEXT NOT NULL,
-    "grade" INTEGER NOT NULL,
+    "grade" DOUBLE PRECISION NOT NULL,
     "created" TIMESTAMP(3) NOT NULL,
 
     CONSTRAINT "coefficients_pkey" PRIMARY KEY ("id")
@@ -60,6 +60,24 @@ CREATE TABLE "teachers" (
 
     CONSTRAINT "teachers_pkey" PRIMARY KEY ("id")
 );
+
+-- CreateIndex
+CREATE UNIQUE INDEX "competences_id_key" ON "competences"("id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "presences_id_key" ON "presences"("id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "coefficients_id_key" ON "coefficients"("id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "graduating_class_id_key" ON "graduating_class"("id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "students_id_key" ON "students"("id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "teachers_id_key" ON "teachers"("id");
 
 -- AddForeignKey
 ALTER TABLE "presences" ADD CONSTRAINT "presences_competence_id_fkey" FOREIGN KEY ("competence_id") REFERENCES "competences"("id") ON DELETE CASCADE ON UPDATE CASCADE;

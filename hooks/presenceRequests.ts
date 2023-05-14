b5886ac8d5e807511899a7f7743f9bee613fed03
Felipi Santana <devfelipisan@gmail.com>
2023-05-14 14:16:30 -0300

    chore: modify presence api

---
+++ hooks/presenceRequests.ts
@@ -0,0 +1,25 @@
+export default async function PresenseList(id?: string) {
+  if (id) {
+    const result = fetch(`/api/presences/${id}`).then(
+      async (response) => await response.json().then((data) => data)
+    );
+    return result;
+  }
+  const result = fetch("/api/presences/all").then(
+    async (response) => await response.json().then((data) => data)
+  );
+
+  return result;
+}
+
+export async function PostPresences(body: any) {
+  const result = fetch("/api/presences/create", {
+    method: "post",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body,
+  });
+  return result;
+}
--- pages/api/presences/[id].ts
+++ pages/api/presences/[id].ts
@@ -6,21 +6,29 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  if (req.method == "GET") {
+  const id = req.query.id?.toString();
+
+  if (req.method == "GET" && id == "all") {
     const presencesList = await prisma.presences.findMany();
     return res.status(200).json(presencesList);
   }
 
-  if (req.method == "POST") {
-    const { student_id, presence, competence_id } = req.body;
+  if (req.method == "GET" && id) {
+    const presencesList = await prisma.presences.findMany({
+      where: { student_id: id },
+      select: {
+        presence: true,
+        student: { select: { name: true, birthday: true } },
+      },
+    });
+    return res.status(200).json(presencesList);
+  }
+
+  if (req.method == "POST" && id == "create") {
+    const { student_id, presence } = req.body;
     const newPresence = await prisma.presences.create({
       data: {
         id: uuidv4(),
-        competence: {
-          connect: {
-            id: competence_id,
-          },
-        },
         student: {
           connect: {
             id: student_id,

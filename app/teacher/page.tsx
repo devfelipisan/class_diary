c80463bc8e4282a1cd6fd30c0294a4ecc73960e9
Felipi Santana <devfelipisan@gmail.com>
2023-05-15 15:41:31 -0300

    fix: fix data fields

--- app/teacher/page.tsx
+++ app/teacher/page.tsx
@@ -87,13 +87,13 @@ export default function Page() {
     return (
       <div className="my-2 rounded-md border-1 shadow-md" key={item.id}>
         <div className="block rounded-md border-1 mx-10 py-2 shadow-sm">
-          <div className="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-6">
+          <div className="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-3">
             <div className="sm:col-span-1">
               <label
                 htmlFor="classSerie"
                 className="block text-sm font-semibold leading-6 text-gray-900"
               >
-                Turma
+                Turma/Série
               </label>
               <div className="relative mt-2.5">
                 <input
@@ -101,8 +101,8 @@ export default function Page() {
                   name="classSerie"
                   id="classSerie"
                   autoComplete="classSerie"
-                  className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                  value={item.school_year}
+                  className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 bg-gray-200 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  value={item.school_series}
                   disabled
                 />
               </div>
@@ -120,18 +120,18 @@ export default function Page() {
                   name="classYear"
                   id="classYear"
                   autoComplete="classYear"
-                  className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                  value={item.school_series}
+                  className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 bg-gray-200 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  value={item.school_year}
                   disabled
                 />
               </div>
             </div>
-            <div className="sm:col-span-2">
+            {/* <div className="sm:col-span-2">
               <label
                 htmlFor="observation"
                 className="block text-sm font-semibold leading-6 text-gray-900"
               >
-                Observação
+                Resumo
               </label>
               <div className="relative mt-2.5">
                 <input
@@ -142,8 +142,8 @@ export default function Page() {
                   className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 />
               </div>
-            </div>
-            <div className="sm:col-span-2 relative mt-10">
+            </div> */}
+            <div className="sm:col-span-1 relative mt-10">
               <button
                 type="submit"
                 className="block w-full rounded-md bg-green-600 px-1 py-1 text-center text-sm font-semibold text-white shadow-sm hover:bg-green-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-green-600"
